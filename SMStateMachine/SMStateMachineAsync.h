eaf00beb97d1d91ceaafb00b317567d685be5104
Artem Kireev <akireev@codemastersintl.com>
2013-07-20 11:50:55 +0400

    Add queue property to AsyncStateMachine

--- SMStateMachine/SMStateMachineAsync.h
+++ SMStateMachine/SMStateMachineAsync.h
@@ -11,6 +11,11 @@
 
 @interface SMStateMachineAsync : SMStateMachine
 
+/**
+ The process events queue. If `NULL` (default), the main queue is used.
+ */
+@property (nonatomic, strong) dispatch_queue_t queue;
+
 -(void)postAsync:(NSString *)event;
 -(NSString *)postAsync:(NSString *)event after:(NSUInteger)milliseconds;
 -(void)dropTimingEvent:(NSString *)eventUuid;
--- SMStateMachine/SMStateMachineAsync.m
+++ SMStateMachine/SMStateMachineAsync.m
@@ -15,9 +15,8 @@
 @implementation SMStateMachineAsync
 
 
-
 - (void)postAsync:(NSString *)event {
-    dispatch_async(dispatch_get_main_queue(), ^{
+    dispatch_async(self.queue, ^{
         [self post: event];
     });
 }
@@ -33,7 +32,7 @@
     NSString *uuid = [self createUuid];
     [self.allowedTimingEvents addObject:uuid];
     dispatch_time_t timeout = dispatch_time(DISPATCH_TIME_NOW, milliseconds * NSEC_PER_MSEC);
-    dispatch_after(timeout, dispatch_get_main_queue(), ^{
+    dispatch_after(timeout, self.queue, ^{
         if ([weakSelf.allowedTimingEvents containsObject:uuid]){
             [weakSelf.allowedTimingEvents removeObject:uuid];
             [weakSelf postAsync:event];
@@ -53,6 +52,13 @@
         _allowedTimingEvents = [[NSMutableArray alloc] init];
     }
     return _allowedTimingEvents;
+}
+
+-(dispatch_queue_t)queue{
+    if (_queue == NULL){
+        _queue = dispatch_get_main_queue();
+    }
+    return _queue;
 }
 
 
